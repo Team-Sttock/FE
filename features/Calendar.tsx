a8652506180b448d6e6ba9bfd8a6dba4f98c392f
Cuzz <rygus9@naver.com>
2023-08-19 00:42:38 +0900

    feat: calendar 모양 형성

--- features/Calendar.tsx
+++ features/Calendar.tsx
@@ -3,22 +3,71 @@ import {
   endOfMonth,
   endOfWeek,
   getDate,
+  getMonth,
+  getYear,
   isSameMonth,
   startOfMonth,
   startOfWeek,
 } from 'date-fns'
 import { chunk } from 'lodash-es'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+
+const WEEKEND = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
 export default function Calendar() {
   const [nowDate, setNowDate] = useState(new Date())
-  const calandarDays = getCalendarDays(nowDate)
-  console.log(calandarDays)
+  const nowDateNum = getDate(nowDate)
+  const nowMonth = getMonth(nowDate)
+  const nowYear = getYear(nowDate)
+  const calandarDays = useMemo(
+    () => getCalendarDays(nowYear, nowMonth),
+    [nowMonth, nowYear]
+  )
 
-  return <div>Calendar</div>
+  return (
+    <article className="w-full h-fit">
+      <header className="w-full">
+        <div className="w-full flex items-stretch">
+          {WEEKEND.map((week) => (
+            <span
+              key={week}
+              className="inline-block text-amber-800 text-base flex-1 text-center py-2"
+            >
+              {week}
+            </span>
+          ))}
+        </div>
+      </header>
+      <section className="w-full">
+        {calandarDays.map((weeks, idx) => (
+          <div key={idx} className="w-full flex items-stretch">
+            {weeks.map((day) => (
+              <div
+                key={`${idx} ${day.date}`}
+                className={`${
+                  day.inMonth ? 'text-gray-800' : 'text-gray-400'
+                } flex-1 py-2 text-center`}
+              >
+                <span
+                  className={`inline-flex items-center justify-center w-8 h-8 rounded-full ${
+                    day.inMonth && day.date === nowDateNum ? 'bg-green-300' : ''
+                  }`}
+                >
+                  {day.date}
+                </span>
+              </div>
+            ))}
+          </div>
+        ))}
+      </section>
+    </article>
+  )
 }
 
-const getCalendarDays = (nowDate: Date) => {
+const getCalendarDays = (year: number, month: number) => {
+  const nowDate = new Date(year, month, 1)
+  console.log(year, month, nowDate)
+
   const daysInCalendar = eachDayOfInterval({
     start: startOfWeek(startOfMonth(nowDate)),
     end: endOfWeek(endOfMonth(nowDate)),
--- pages/test.tsx
+++ pages/test.tsx
@@ -2,7 +2,7 @@ import Calendar from '@/features/Calendar'
 
 export default function Page() {
   return (
-    <div>
+    <div className="w-[600px]">
       <Calendar></Calendar>
     </div>
   )
