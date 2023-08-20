93b6be47f7a8f72db224b8243585f329c5668936
yeokyoung song <spolice0324@gmail.com>
2023-08-20 23:01:57 +0900

    chore : font 불러오기, tailwind config 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +6,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'nexon': ['NEXON LV2 Gothic', 'sans-serif'],
+        'hahmlet': ['Hahmlet', 'sans-serif'],
+        'kimjungchul': ['Kimjungchul Gothic', 'sans-serif'],
+      },
       colors: {
         "light-brown": "#9f8772",
         "dark-brown": "#665a48",
         "beige": "#ebe4d9",
         "ivory": "#f6f4ef",
-      }
+      },
     },
   },
   plugins: [],
