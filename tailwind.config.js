15bead846381b6fa614aaaa42498270a77dc4a48
yeokyoung song <spolice0324@gmail.com>
2023-08-20 19:36:50 +0900

    chore : tailwind.config에 커스텀 컬러 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,15 @@ module.exports = {
     './features/**/*.{js,ts,jsx,tsx,mdx}',
     './common/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        "light-brown": "#9f8772",
+        "dark-brown": "#665a48",
+        "beige": "#ebe4d9",
+        "ivory": "#f6f4ef",
+      }
+    },
+  },
   plugins: [],
 }
