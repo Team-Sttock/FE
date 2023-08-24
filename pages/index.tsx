09331a71032f472c2a7a20d1a03fdce6f06749c8
yeokyoung song <spolice0324@gmail.com>
2023-08-24 17:24:00 +0900

    feature: 메인페이지 섹션 1~3 반응형 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,44 +2,46 @@ export default function Home() {
   return (
     <main>
       <nav className="w-full h-[80px]">네브바</nav>
-      {/* 메인1 - 메인화면 */}
+      {/* 메인1-1 - Sttock */}
       <section>
-        <div className="flex flex-col">
-          <div className="relative">
-            <img
-              src="/main/main-img-1.svg"
-              alt="main-img-1"
-              className=" w-full h-full"
-            />
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-              <h2 className="whitespace-nowrap font-kimjungchul font-bold text-md md:text-lg  text-ivory">
-                스스로 똑똑하게,
-              </h2>
-              <h1 className="whitespace-nowrap font-kimjungchul font-bold  text-4xl md:text-6xl  text-ivory uppercase">
-                sttock
-              </h1>
-            </div>
-          </div>
-          <div className=" min-h-[300px] bg-ivory flex flex-col items-center justify-center text-center">
-            <h1 className="whitespace-nowrap mb-6 font-hahmlet text-2xl md:text-4xl text-light-brown font-extralight">
-              &quot;잊고사세요, 스똑이 알려드릴게요. &quot;
+        <div className="relative">
+          <img
+            src="/main/main-img-1.png"
+            alt="main-img-1"
+            className=" w-screen md:h-[500px] sm:h-full   object-cover"
+          />
+
+          <div className=" container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <h2 className="font-kimjungchul font-bold text-md md:text-lg text-ivory">
+              스스로 똑똑하게,
+            </h2>
+            <h1 className=" font-kimjungchul font-bold  text-4xl md:text-6xl  text-ivory uppercase">
+              sttock
             </h1>
-            <p className="whitespace-nowrap font-nexon text-lg md:text-2xl text-dark-brown font-thin ">
-              구매주기계산부터 <br /> 재구매일확인까지 <br /> 스똑에서 해결할 수
-              있습니다.
-            </p>
           </div>
+        </div>
+      </section>
+      {/* 메인1-2 - 잊고사세요 */}
+      <section>
+        <div className="min-h-[250px] md:h-[350px] bg-ivory flex flex-col items-center justify-center text-center">
+          <h1 className=" p-4 mb-6 font-hahmlet text-xl sm:text-2xl md:text-4xl  text-light-brown font-extralight">
+            &quot;잊고사세요, 스똑이 알려드릴게요. &quot;
+          </h1>
+          <p className=" font-nexon text-md sm:text-lg  md:text-2xl text-dark-brown font-thin ">
+            구매주기계산부터 <br /> 재구매일확인까지 <br /> 스똑에서 해결할 수
+            있습니다.
+          </p>
         </div>
       </section>
 
       {/* 메인2 - 이번주 구매 */}
       <section>
         <div className="flex flex-wrap bg-beige">
-          <div className="relative w-full  md:w-1/2">
+          <div className="relative w-full md:w-1/2">
             <img
               src="/main/main-img-2.png"
               alt="main-img-2"
-              className=" md:h-screen h-[750px] w-screen  object-cover"
+              className=" h-[350px] w-full md:h-full lg:w-screen  object-cover"
             />
             <div className="absolute flex flex-col items-center justify-center text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <h2 className="whitespace-nowrap font-kimjungchul font-bold text-sm md:text-lg text-beige uppercase">
@@ -50,16 +52,15 @@ export default function Home() {
               </h1>
             </div>
           </div>
-          <div className="w-full md:w-1/2">
-            <div className="flex flex-col items-left pl-10 py-20">
-              <div className="mb-60">
-                <p className="whitespace-nowrap font-hahmlet text-md sm:text-3xl lg:text-4xl text-light-brown font-extralight">
-                  &quot;거의 다 사용한 <br />
-                  생활용품이 무엇이었는지
-                  <br /> 스똑이 보여드려요.&quot;
-                </p>
-              </div>
-              <p className="whitespace-nowrap font-nexon text-xs sm:text-lg lg:text-2xl text-dark-brown font-thin">
+          <div className="w-full md:w-1/2 p-10 flex flex-col justify-between">
+            <div className="p-4">
+              <h1 className="mb-20 md:mb-60 mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
+                &quot;거의 다 사용한 <br />
+                생활용품이 무엇이었는지
+                <br /> 스똑이 보여드려요.&quot;
+              </h1>
+
+              <p className="  font-nexon text-xs sm:text-lg lg:text-xl text-dark-brown font-thin">
                 &quot;다 써서 사야하는게 있었는데... 뭐였더라.&quot; <br />
                 <br />
                 마트나 상품점에서 <br /> 뭘 사야했었는지 고민하지마세요. <br />
@@ -72,18 +73,16 @@ export default function Home() {
       </section>
 
       {/* 메인 3 - 스똑 캘린더 */}
-
       <section>
         <div className="flex flex-wrap bg-beige">
-          <div className="w-full md:w-1/2 ">
-            <div className="flex flex-col items-left px-5 py-20">
-              <div className="mb-60">
-                <p className="whitespace-nowrap font-hahmlet text-md sm:text-3xl lg:text-4xl text-light-brown font-extralight">
-                  &quot;우리집 생활용품 달력, <br />
-                  다음을 미리 준비할 수 있도록 &quot;
-                </p>
-              </div>
-              <p className="whitespace-nowrap font-nexon text-xs sm:text-lg lg:text-2xl text-dark-brown font-thin">
+          <div className="w-full md:w-1/2 p-10 flex flex-col justify-between">
+            <div className="p-4">
+              <h1 className="mb-20 md:mb-60 mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
+                &quot;우리집 생활용품 달력, <br />
+                다음을 미리 준비할 수 있도록 &quot;
+              </h1>
+
+              <p className="  font-nexon text-xs sm:text-lg lg:text-xl text-dark-brown font-thin">
                 다음 주 그리고 다음 달에 <br />
                 어떤 생활용품이 소진되는지
                 <br />
@@ -94,11 +93,11 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="relative w-full  md:w-1/2">
+          <div className="relative w-full md:w-1/2">
             <img
               src="/main/main-img-3.png"
               alt="main-img-3"
-              className=" md:h-screen h-[750px] w-screen  object-cover"
+              className=" h-[350px] w-full md:h-full lg:w-screen  object-cover"
             />
             <div className="absolute flex flex-col items-center justify-center text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <h2 className="whitespace-nowrap font-kimjungchul font-bold text-sm md:text-lg text-beige uppercase">
@@ -114,55 +113,55 @@ export default function Home() {
 
       {/* 메인 4 - 항목별 보기 */}
       <section>
-        <div>
-          <div className="w-screen h-screen flex flex-wrap items-center justify-center bg-beige">
-            <div className="w-1/3 p-10">
-              <div className="">
-                <p className="mb-60 whitespace-nowrap font-hahmlet text-sm md:text-2xl text-light-brown font-extralight">
-                  &quot;우리집 생활용품을 <br /> 매장에서처럼 <br /> 한눈에
-                  관리해보세요.&quot;
-                </p>
-                <p className="font-nexon text-xs sm:text-xs lg:text-lg text-dark-brown font-thin">
-                  집에서 사용하는 생활용품을 <br /> 체계적으로 관리할 수
-                  있습니다. <br />
-                  어떤 생활용품을 자주 구매하는지,
-                  <br /> 그리고 무엇을 빨리 소진하는지 <br /> 한눈에
-                  확인해보세요!
-                </p>
-              </div>
+        <div className="flex items-center justify-center bg-beige">
+        
+          <div className="w-full lg:w-1/3">
+            <div className="">
+              <p className="mb-60 whitespace-nowrap font-hahmlet text-sm md:text-2xl text-light-brown font-extralight">
+                &quot;우리집 생활용품을 <br /> 매장에서처럼 <br /> 한눈에
+                관리해보세요.&quot;
+              </p>
+              <p className="font-nexon text-xs sm:text-xs lg:text-lg text-dark-brown font-thin">
+                집에서 사용하는 생활용품을 <br /> 체계적으로 관리할 수 있습니다.{' '}
+                <br />
+                어떤 생활용품을 자주 구매하는지,
+                <br /> 그리고 무엇을 빨리 소진하는지 <br /> 한눈에 확인해보세요!
+              </p>
             </div>
-            <div className=" relative  w-1/3 ">
-              <img
-                src="/main/main-img-4.png"
-                alt="main-img-4"
-                className="mx-auto h-[580px] w-[420px] object-cover"
-              />
-              <div className="absolute flex flex-col items-center justify-center text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <h2 className="font-kimjungchul font-bold text-sm md:text-lg text-beige uppercase">
-                  view
-                </h2>
-                <h1 className="font-kimjungchul font-bold text-2xl md:text-4xl  text-beige ">
-                  항목별보기
-                </h1>
-              </div>
+          </div>
+        
+        
+          <div className=" relative w-full lg:w-1/3 ">
+            <img
+              src="/main/main-img-4.png"
+              alt="main-img-4"
+              className="h-[580px] w-[420px] object-cover"
+            />
+            <div className="absolute flex flex-col items-center justify-center text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <h2 className=" whitespace-nowrap font-kimjungchul font-bold text-sm md:text-lg text-beige uppercase">
+                view
+              </h2>
+              <h1 className="whitespace-nowrap font-kimjungchul font-bold text-2xl md:text-4xl  text-beige ">
+                항목별보기
+              </h1>
             </div>
-            <div className="w-1/3 mt-40 p-20">
-              <div className="flex justify-center items-center mb-10">
-                <img src="/main/kitchen_item_icon.svg" alt="주방아이콘" />
-                <hr className=" mx-4 border-t-1 w-[100px] border-light-brown"></hr>
-                <p className="whitespace-nowrap font-nexon text-sm md:text-md font-extralight text-dark-brown">
-                  주방세제
-                </p>
+          </div>
+          <div className="w-full lg:w-1/3 p-20">
+            <div className="flex justify-center items-center mb-10">
+              <img src="/main/kitchen_item_icon.svg" alt="주방아이콘" />
+              <hr className=" mx-4 border-t-1 w-[100px] border-light-brown"></hr>
+              <p className="whitespace-nowrap font-nexon text-sm md:text-md font-extralight text-dark-brown">
+                주방세제
+              </p>
+            </div>
+            <div>
+              <div className=" md:w-50 w-30  h-5  rounded-full shadow-lg bg-ivory">
+                <div className="h-full w-[40%] rounded-full bg-light-brown  "></div>
               </div>
-              <div>
-                <div className=" md:w-50 w-30  h-5  rounded-full shadow-lg bg-ivory">
-                  <div className="h-full w-[40%] rounded-full bg-light-brown  "></div>
-                </div>
 
-                <p className=" text-xs md:text-sm mt-4 font-nexon font-light  text-dark-brown">
-                  총 2L 중에 820ml 소진했어요!
-                </p>
-              </div>
+              <p className=" text-xs md:text-sm mt-4 font-nexon font-light  text-dark-brown">
+                총 2L 중에 820ml 소진했어요!
+              </p>
             </div>
           </div>
         </div>
