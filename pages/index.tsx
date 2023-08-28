bd6b2760598f4e4f4608169929f06f3c476e1825
yeokyoung song <spolice0324@gmail.com>
2023-08-28 16:38:17 +0900

    featue: 메인페이지 모바일 뷰 디자인 수정 사항 반영

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,8 +55,8 @@ export default function Home() {
             </div>
           </div>
           <div className="w-full md:w-1/2 p-10 flex-col justify-between">
-            <div className="p-4">
-              <h1 className="mb-20 md:mb-60 mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
+            <div className="p-4 text-center md:text-left">
+              <h1 className="mb-20 md:mb-60 md:mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
                 &quot;거의 다 사용한 <br />
                 생활용품이 무엇이었는지
                 <br /> 스똑이 보여드려요.&quot;
@@ -77,37 +77,37 @@ export default function Home() {
       {/* 메인 3 - 스똑 캘린더 */}
       <section>
         <div className="flex flex-wrap bg-beige">
-          <div className="w-full md:w-1/2 p-10 flex-col justify-between">
-            <div className="p-4">
-              <h1 className="mb-20 md:mb-60 mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
-                &quot;우리집 생활용품 달력, <br />
-                다음을 미리 준비할 수 있도록 &quot;
-              </h1>
-
-              <p className="  font-nexon text-xs sm:text-lg lg:text-xl text-dark-brown font-thin">
-                다음 주 그리고 다음 달에 <br />
-                어떤 생활용품이 소진되는지
-                <br />
-                한 눈에 볼 수 있어요.
-                <br />
-                혹시 다음 주에 마트에 갈 예정이라면, <br />
-                스똑 캘린더에서 미리 확인해보세요.
-              </p>
-            </div>
-          </div>
-          <div className="relative w-full md:w-1/2">
+          <div className="md:order-2 w-full md:w-1/2 relative">
             <img
               src="/main/main-img-3.png"
               alt="main-img-3"
-              className="h-[350px] w-full md:h-screen lg:w-screen  object-cover object-center"
+              className="h-[350px] w-full md:h-screen lg:w-screen object-cover object-center"
             />
             <div className="absolute flex flex-col items-center justify-center text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <h2 className="whitespace-nowrap font-kimjungchul font-bold text-sm md:text-lg text-beige uppercase">
                 calendar
               </h2>
-              <h1 className="whitespace-nowrap font-kimjungchul font-bold text-2xl md:text-4xl  text-beige">
+              <h1 className="whitespace-nowrap font-kimjungchul font-bold text-2xl md:text-4xl text-beige">
                 스똑 캘린더
               </h1>
+            </div>
+          </div>
+          <div className="md:order-1 w-full md:w-1/2 p-10 flex-col justify-between">
+            <div className="p-4 text-center md:text-left">
+              <h1 className="mb-20 md:mb-60 md:mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
+                &quot;우리집 생활용품 달력, <br />
+                다음을 미리 준비할 수 있도록 &quot;
+              </h1>
+              <p className="font-nexon text-xs sm:text-lg lg:text-xl text-dark-brown font-thin ">
+                다음 주 그리고 다음 달에
+                <br />
+                어떤 생활용품이 소진되는지
+                <br />한 눈에 볼 수 있어요.
+                <br />
+                혹시 다음 주에 마트에 갈 예정이라면,
+                <br />
+                스똑 캘린더에서 미리 확인해보세요.
+              </p>
             </div>
           </div>
         </div>
@@ -116,27 +116,12 @@ export default function Home() {
       {/* 메인 4 - 항목별 보기 */}
       <section>
         <div className="flex flex-col md:flex-row items-center justify-center bg-beige">
-          <div className="w-full md:w-1/3 p-10 flex flex-col justify-between">
-            <div className="p-4">
-              <p className="mb-24 md:mb-60 mt-10 font-hahmlet text-md sm:text-xl lg:text-3xl text-light-brown font-extralight">
-                &quot;우리집 생활용품을 <br /> 매장에서처럼 <br /> 한눈에
-                관리해보세요.&quot;
-              </p>
-              <p className="font-nexon text-xs sm:text-lg lg:text-xl text-dark-brown font-thin">
-                집에서 사용하는 생활용품을 <br /> 체계적으로 관리할 수 있습니다.{' '}
-                <br />
-                어떤 생활용품을 자주 구매하는지,
-                <br /> 그리고 무엇을 빨리 소진하는지 <br /> 한눈에 확인해보세요!
-              </p>
-            </div>
-          </div>
-
-          <div className="relative w-full md:w-1/3 ">
+          <div className="relative md:order-2 w-full md:w-1/3 ">
             <div className="flex justify-center ">
               <img
                 src="/main/main-img-4.png"
                 alt="main-img-4"
-                className=" h-[500px] lg:h-full object-cover p-10"
+                className=" h-[350px] md:h-screen w-full object-cover md:p-10"
               />
             </div>
             <div className="absolute flex flex-col items-center justify-center text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -148,21 +133,41 @@ export default function Home() {
               </h1>
             </div>
           </div>
-          <div className="w-full p-10 md:w-1/3">
-            <div className="flex flex-col justify-start p-2 pl-6 mt-20 md:mt-60 ">
-              <div className="flex flex-row items-center mb-10">
-                <img src="/main/kitchen_item_icon.svg" alt="주방아이콘" />
-                <hr className=" mx-4 border-t-1 w-1/4 border-light-brown"></hr>
+          <div className="w-full md:w-1/3 md:order-1  flex flex-col justify-between">
+            <div className="p-10 md:pr-2 text-center md:text-left">
+              <p className="mb-24 md:mb-60 md:mt-10 font-hahmlet text-md sm:text-lg lg:text-3xl text-light-brown font-extralight">
+                &quot;우리집 생활용품을 <br /> 매장에서처럼 <br /> 한눈에
+                관리해보세요.&quot;
+              </p>
+              <p className="font-nexon text-xs sm:text-md lg:text-xl text-dark-brown font-thin">
+                집에서 사용하는 생활용품을 <br /> 체계적으로 관리할 수 있습니다.{' '}
+                <br />
+                어떤 생활용품을 자주 구매하는지,
+                <br /> 그리고 무엇을 빨리 소진하는지 <br /> 한눈에 확인해보세요!
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full hp-10 md:order-3 md:w-1/3">
+            <div className="flex flex-col items-center md:justify-center justify-start md:mt-60 ">
+              <div className="flex flex-row items-center pb-4">
+                <img
+                  src="/main/kitchen_item_icon.svg"
+                  alt="주방아이콘"
+                />
+                <hr className="px-6 mx-3 border-t-1 w-1/4 border-light-brown"></hr>
                 <p className="font-nexon text-xs md:text-md font-extralight text-dark-brown">
                   주방세제
                 </p>
               </div>
-              <div className=" w-1/2 h-5  rounded-full shadow-lg bg-ivory">
-                <div className="h-full w-[40%] rounded-full bg-light-brown  "></div>
+              <div className="">
+                <div className="h-4  rounded-full shadow-lg bg-ivory">
+                  <div className="h-full w-[40%] rounded-full bg-light-brown  "></div>
+                </div>
+                <p className=" text-xs md:text-sm mt-4 font-nexon font-light  text-dark-brown">
+                  총 2L 중에 820ml 소진했어요!
+                </p>
               </div>
-              <p className=" text-xs md:text-sm mt-4 font-nexon font-light  text-dark-brown">
-                총 2L 중에 820ml 소진했어요!
-              </p>
             </div>
           </div>
         </div>
@@ -175,7 +180,6 @@ export default function Home() {
           alt="main-img-5"
           className="hidden md:block"
         />
-        {/* md 사이즈 이하 모바일용 사진 보이기 */}
       </section>
 
       <footer className="h-[300px] bg-beige">푸터</footer>
