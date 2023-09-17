73763f562e8a36f6635756db7a9d2c96b3326f2c
Cuzz <rygus9@naver.com>
2023-09-17 20:30:47 +0900

    refactor: 잘못 들어간 Auth 제거

--- features/auth/authApis.ts
+++
@@ -1,9 +0,0 @@
-import { client } from '@/features/common/apis/client'
-
-interface GetLoginProps {
-  login_id: string
-  password: string
-}
-
-export const getLogin = async ({ login_id, password }: GetLoginProps) =>
-  await client.post<GetLoginProps>('/login', { login_id, password })
