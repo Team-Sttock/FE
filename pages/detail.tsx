bcfb2d885323641c46ccf7fc7b1e15b7ef9666db
yeokyoung song <spolice0324@gmail.com>
2023-09-24 01:17:18 +0900

    feat : 스타일링 피드백 요구사항 반영

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -43,8 +43,9 @@ export default function Page() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center m-auto max-w-3xl w-2/3 min-w-[80%]">
-        <div className="flex flex-col justify-center items-stretch my-10 max-w-3xl w-full p-2">
+
+      <div className="flex flex-col items-center justify-center m-auto max-w-3xl w-[95%] min-w-[90%]">
+        <div className="flex flex-col justify-center items-stretch my-10 max-w-3xl w-[90%] p-2">
           <h1
             className={classNames(
               'md:text-2xl text-xl font-bold text-dark-brown mb-2',
@@ -56,23 +57,23 @@ export default function Page() {
           <hr className="relative w-full border-1 border-beige " />
         </div>
 
-        <div className=" border border-beige flex sm:flex-row flex-col items-cetenr w-[90%] justify-around max-w-3xl m-auto px-4 py-6 lg:px-10 ">
-          <div className="relative flex flex-row justify-center items-center sm:px-2 sm:mb-0 mb-4 m-auto">
+        <div className="border border-beige flex sm:flex-row flex-col items-center justify-around max-w-2xl m-auto w-[80%] md:py-6 sm:px-0 px-4 py-4 min-w-fit-content">
+          <div className="relative flex flex-row justify-center items-center m-auto mb-4 sm:mb-0">
             <img
               src="/icons/kitchen-icon.svg"
               alt="icon"
               className="w-10 h-10 md:w-12 md:h-12 lg:mr-8 mr-3"
             />
-            <p className="text-dark-brown text-sm sm:text-md md:text-xl font-bold whitespace-nowrap">
+            <p className="text-dark-brown text-sm sm:text-md md:text-xl font-bold whitespace-nowrap mr-2">
               {data.name}
             </p>
-            <span className="text-dark-brown md:pl-3 text-sm s sm:text-md overflow-hidden whitespace-nowrap overflow-ellipsis px-2">
+            <span className="text-dark-brown text-sm sm:text-md overflow-hidden whitespace-nowrap overflow-ellipsis mx-2">
               {data.nickname}
             </span>
           </div>
 
-          <div className="relative flex flex-row items-center sm:space-x-1 m-auto">
-            <div className="w-20 sm:w-32 md:w-60 h-3 bg-beige rounded-full mr-2 ">
+          <div className="relative flex flex-wrap items-center sm:space-x-1 m-auto">
+            <div className="w-20 sm:w-32 md:w-52 h-3 bg-ivory rounded-full mr-2 ">
               <div className="w-[20%] h-3 bg-light-brown rounded-full"></div>
             </div>
 
@@ -85,53 +86,55 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="flex items-center justify-center mt-10 mb-8 sm:mb-10 mx-10  ">
-          <div className="justify-center flex flex-wrap sm:flex-row">
-            <div className="relative flex-row flex mx-2 sm:mr-10 sm:mb-0 mb-4">
-              <p className="text-dark-brown md:text-md text-sm font-bold">
-                재고 정보
-              </p>
-            </div>
-            <div className="flex flex-col justify-between items-stretch text-dark-brown sm:text-md text-sm sm:space-y-4">
-              <p>
-                전체
-                <span className="text-light-brown inline"> {data.amount} </span>
-                의 예상 소진일은
-                <span className="text-light-brown inline mx-1">
-                  {data.expectionDate}
-                </span>
-                입니다.
-              </p>
-              <br />
-              <p>
-                카테고리 :
-                <span className="text-light-brown inline ml-1">
-                  {data.category}
-                </span>
-              </p>
-              <br />
-              <p>
-                구매일 :
-                <span className="text-light-brown inline ml-1 md:mr-4 mr-1">
-                  {data.purchaseDate}
-                </span>
-                <span className="text-light-brown inline">{pastDays}</span>일
-                경과
-              </p>
-              <br />
-              <p>
-                유통기한 :
-                <span className="text-light-brown inline ml-1">
-                  {data.expirationDate}
-                </span>
-              </p>
-            </div>
+        <div className="flex justify-center w-[80%] mt-10 mb-8 sm:mb-10 flex-wrap sm:flex-row p-4">
+          <div className="flex mx-10 sm:mb-0 mb-4 pb-4">
+            <p className="text-dark-brown md:text-md text-sm font-bold">
+              재고 정보
+            </p>
+          </div>
+          <div className="flex flex-col text-dark-brown sm:text-md text-sm sm:space-y-4 sm:mt-0">
+            <p>
+              전체
+              <span className="text-light-brown inline mx-4">
+                {data.amount}
+              </span>
+              의 예상 소진일은
+              <span className="text-light-brown inline mx-4">
+                {data.expectionDate}
+              </span>
+              입니다.
+            </p>
+            <br />
+            <p>
+              카테고리 :
+              <span className="text-light-brown inline mx-4">
+                {data.category}
+              </span>
+            </p>
+            <br />
+            <p>
+              구매일 :
+              <span className="text-light-brown inline mx-4 ">
+                {data.purchaseDate}
+              </span>
+              <span className="text-light-brown inline mx-4">{pastDays}</span>일
+              경과
+            </p>
+            <br />
+            <p>
+              유통기한 :
+              <span className="text-light-brown inline mx-4">
+                {data.expirationDate}
+              </span>
+            </p>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center w-full space-y-7 md:space-y-0 mt-10 mb-20 max-w-3xl m-auto">
-          <Button className="w-28 h-8 p-1 sm:m-auto ">상태변경하기</Button>
-          <Button className="w-28 h-8 p-1 sm:m-auto ">수정하기</Button>
-          <Button className="w-28 h-8 p-1 sm:m-auto ">삭제하기</Button>
+        <div className="flex flex-col sm:flex-row items-center justify-center w-[70%] space-y-7 sm:space-y-0 mb-10 mt-4 max-w-3xl m-auto p-4">
+          <Button className="w-28 h-8 sm:m-auto font-normal">
+            상태변경하기
+          </Button>
+          <Button className="w-28 h-8 sm:m-auto font-normal">수정하기</Button>
+          <Button className="w-28 h-8 sm:m-auto font-normal">삭제하기</Button>
         </div>
       </div>
       <footer className="h-[300px] bg-ivory">푸터</footer>
