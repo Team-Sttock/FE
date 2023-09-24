461bb076e701be188c754e49db8af8b28a9071f6
yeokyoung song <spolice0324@gmail.com>
2023-09-24 20:34:13 +0900

    feat : box shadow tailwind config에 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
         "ivory": "#f6f4ef",
         "mint": "#c8dbbe",
       },
+      boxShadow: {
+        custom: '0px 4px 4px 0 rgba(0, 0, 0, 0.15)',
+      },
     },
   },
   plugins: [],
