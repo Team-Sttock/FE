63c58c81f9b3d7efd1e33d07b56d96f1d7134ad9
Cuzz <rygus9@naver.com>
2023-11-05 13:21:15 +0900

    feat: axios memoize 하기

--- apis/index.ts
+++ apis/index.ts
@@ -1,12 +1,17 @@
 import axios from 'axios'
+import { memoize } from 'lodash-es'
 
-export const client = axios.create({
-  baseURL: 'https://api.sttock.co.kr',
-})
+export const client = memoize(
+  axios.create({
+    baseURL: 'https://api.sttock.co.kr',
+  })
+)
 
-export const userClient = axios.create({
-  baseURL: 'https://api.sttock.co.kr/api/v1/user',
-})
+export const userClient = memoize(
+  axios.create({
+    baseURL: 'https://api.sttock.co.kr/api/v1/user',
+  })
+)
 
 export interface MutationRes {
   message: string
