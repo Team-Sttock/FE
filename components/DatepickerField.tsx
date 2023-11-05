3ac3a58bbd6dc5eb5eec5ec00d71d79b05f6348d
yeokyoung song <spolice0324@gmail.com>
2023-11-05 14:30:56 +0900

    fix: datePickerField ref 에러 해결

--- components/DatepickerField.tsx
+++ components/DatepickerField.tsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/20/solid'
 import { getMonth, getYear } from 'date-fns'
 import ko from 'date-fns/locale/ko'
 import { range } from 'lodash-es'
-import React from 'react'
+import React, { forwardRef } from 'react'
 import DatePicker, { registerLocale } from 'react-datepicker'
 import { type Control, Controller } from 'react-hook-form'
 
@@ -24,99 +24,99 @@ const YEARS = Array.from(
 )
 const MONTHS = range(0, 12)
 
-const DatePickerField = ({
-  control,
-  name,
-  placeholder,
-}: DatePickerFieldProps) => {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field: { onChange, value } }) => (
-        <DatePicker
-          selected={value}
-          onChange={(data: any) => {
-            onChange(data)
-          }}
-          dateFormat="yyyy년 MM월 dd일"
-          dateFormatCalendar="yyyy년 MM월"
-          locale="ko"
-          placeholderText={placeholder ?? '날짜를 선택해주세요'}
-          className="flex items-center justify-center p-3 w-full h-10 border-beige border outline-none text-md text-dark-brown focus:outline-none placeholder:text-beige placeholder:text-sm"
-          popperClassName="w-full max-w-xs relative"
-          wrapperClassName="w-full"
-          calendarContainer={({ children }) => {
-            console.log(children)
-            return (
-              <div
-                className={classNames(
-                  'relative inline-block w-full border border-light-brown bg-white'
-                )}
-              >
-                {children}
-              </div>
-            )
-          }}
-          renderCustomHeader={({
-            date,
-            changeYear,
-            changeMonth,
-            decreaseMonth,
-            increaseMonth,
-            prevMonthButtonDisabled,
-            nextMonthButtonDisabled,
-          }) => (
-            <div className="px-6 py-2 flex items-center justify-between">
-              <button
-                type="button"
-                onClick={decreaseMonth}
-                disabled={prevMonthButtonDisabled}
-              >
-                <ArrowLeftIcon className="w-6 h-6 text-dark-brown" />
-              </button>
-              <div>
-                <select
-                  value={getMonth(date)}
-                  className="bg-transparent text-dark-brown font-bold"
-                  onChange={({ target: { value } }) => {
-                    changeMonth(+value)
-                  }}
+// eslint-disable-next-line react/display-name
+const DatePickerField = forwardRef<any, DatePickerFieldProps>(
+  ({ control, name, placeholder, rules }, ref) => {
+    return (
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field: { onChange, value } }) => (
+          <DatePicker
+            selected={value}
+            onChange={(data: any) => {
+              onChange(data)
+            }}
+            dateFormat="yyyy년 MM월 dd일"
+            dateFormatCalendar="yyyy년 MM월"
+            locale="ko"
+            placeholderText={placeholder ?? '날짜를 선택해주세요'}
+            className="flex items-center justify-center p-3 w-full h-10 border-beige border outline-none text-md text-dark-brown focus:outline-none placeholder:text-beige placeholder:text-sm"
+            popperClassName="w-full max-w-xs relative"
+            wrapperClassName="w-full"
+            calendarContainer={({ children }) => {
+              console.log(children)
+              return (
+                <div
+                  className={classNames(
+                    'relative inline-block w-full border border-light-brown bg-white'
+                  )}
                 >
-                  {MONTHS.map((option) => (
-                    <option key={option} value={option}>
-                      {option + 1}
-                    </option>
-                  ))}
-                </select>
-                <select
-                  value={getYear(date)}
-                  className="bg-transparent text-dark-brown font-bold"
-                  onChange={({ target: { value } }) => {
-                    changeYear(+value)
-                  }}
+                  {children}
+                </div>
+              )
+            }}
+            renderCustomHeader={({
+              date,
+              changeYear,
+              changeMonth,
+              decreaseMonth,
+              increaseMonth,
+              prevMonthButtonDisabled,
+              nextMonthButtonDisabled,
+            }) => (
+              <div className="px-6 py-2 flex items-center justify-between">
+                <button
+                  type="button"
+                  onClick={decreaseMonth}
+                  disabled={prevMonthButtonDisabled}
                 >
-                  {YEARS.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
+                  <ArrowLeftIcon className="w-6 h-6 text-dark-brown" />
+                </button>
+                <div>
+                  <select
+                    value={getMonth(date)}
+                    className="bg-transparent text-dark-brown font-bold"
+                    onChange={({ target: { value } }) => {
+                      changeMonth(+value)
+                    }}
+                  >
+                    {MONTHS.map((option) => (
+                      <option key={option} value={option}>
+                        {option + 1}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    value={getYear(date)}
+                    className="bg-transparent text-dark-brown font-bold"
+                    onChange={({ target: { value } }) => {
+                      changeYear(+value)
+                    }}
+                  >
+                    {YEARS.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <button
+                  type="button"
+                  onClick={increaseMonth}
+                  className={''}
+                  disabled={nextMonthButtonDisabled}
+                >
+                  <ArrowRightIcon className="w-6 h-6 text-dark-brown" />
+                </button>
               </div>
-              <button
-                type="button"
-                onClick={increaseMonth}
-                className={''}
-                disabled={nextMonthButtonDisabled}
-              >
-                <ArrowRightIcon className="w-6 h-6 text-dark-brown" />
-              </button>
-            </div>
-          )}
-        />
-      )}
-    />
-  )
-}
+            )}
+          />
+        )}
+      />
+    )
+  }
+)
 
 export default DatePickerField
