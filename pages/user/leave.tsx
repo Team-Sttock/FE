bb524772ae9dba2a82217581d012352f61351836
yeokyoung song <spolice0324@gmail.com>
2023-11-21 20:43:45 +0900

    feat : 회원 탈퇴 페이지 UI 추가, 탈퇴 사유 제외

---
+++ pages/user/leave.tsx
@@ -0,0 +1,89 @@
+import { Noto_Sans } from 'next/font/google'
+import { useForm } from 'react-hook-form'
+
+import Button from '@/components/Button'
+import Navbar from '@/components/Navbar'
+import { classNames } from '@/utils/classNames'
+
+const NotoSans = Noto_Sans({
+  weight: ['500', '400'],
+  subsets: ['latin'],
+})
+
+const leaveReason = {
+  1: '서비스 이용에 불편함을 겪었습니다.',
+  2: '스똑의 기능을 이용하기 어렵습니다.',
+  3: '스똑의 디자인이 마음에 들지 않습니다.',
+  4: '스똑의 기능이 마음에 들지 않습니다.',
+  5: '스똑의 가격이 마음에 들지 않습니다.',
+  6: '스똑의 기타 이유로 인해 이용을 중단하고 싶습니다.',
+}
+
+interface LeaveReasonFormData {
+  leaveReason: string
+}
+
+export default function Page() {
+  const { control, handleSubmit } = useForm<LeaveReasonFormData>()
+
+  const onSubmit: SubmitHandler<LeaveReasonFormData> = (data) => {
+    console.log(data)
+  }
+
+  const handleLeaveReasonChange = (value: any) => {
+    console.log('value', value)
+  }
+
+  return (
+    <>
+      <Navbar />
+      <main className="m-auto max-w-5xl w-full px-4 mb-10">
+        <header className="mt-10 mb-4 w-full py-2">
+          <div
+            className={classNames(
+              'space-y-2 w-full mb-2',
+              'sm:space-x-4 sm:space-y-0 sm:flex sm:justify-start sm:items-center'
+            )}
+          >
+            <h1
+              className={classNames(
+                'md:text-2xl text-xl font-bold text-dark-brown',
+                NotoSans.className
+              )}
+            >
+              회원탈퇴
+            </h1>
+          </div>
+          <hr className="relative w-full border-1 border-beige" />
+        </header>
+        <div className="flex flex-col justify-center items-center space-y-4">
+          <div className="w-full max-w-3xl m-auto py-8 px-7 flex-col justify-center items-center border border-beige bg-ivory space-y-2">
+            <span className="font-bold text-lg text-dark-brown">안내사항</span>
+            <div className="flex flex-col items-start text-left gap-2 text-sm text-light-brown ">
+              <p>
+                스똑 이용 중 있었던 불편한 점이나 개선할 점을 알려주시면
+                반영하여 더 좋은 서비스로 다시 찾아 뵙겠습니다.
+              </p>
+              <p>아래의 내용을 확인해주시기 바랍니다. </p>
+              <p>
+                1. 개인정보처리방침에 의거해 서비스 이용기록 및 저장된 재고현황
+                등의 모든 정보가 삭제되며 복구할 수 없습니다.
+              </p>
+              <p>
+                2. 탈퇴 신청한 계정의 이메일로 즉시 재가입이 불가능합니다. 단,
+                탈퇴일 이후 3개월이 지나면 가입할 수 있습니다.
+              </p>
+            </div>
+          </div>
+          <div>
+            <span className="text-lg text-dark-brown">탈퇴 사유</span>
+          </div>
+          <div className="space-x-10">
+            <Button className="px-2 py-1">탈퇴하기</Button>
+            <Button className="px-2 py-1">돌아가기</Button>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
