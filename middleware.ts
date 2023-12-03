c7a9e53a55b5eec4db793db094dd7bf4534fc938
Cuzz <rygus9@naver.com>
2023-12-03 16:42:39 +0900

    feat: middleware에 refresh 로직 추가

--- middleware.ts
+++ middleware.ts
@@ -7,11 +7,34 @@ export async function middleware(req: NextRequest) {
     const { status } = await fetch('https://api.sttock.co.kr/api/v1/user/me', {
       headers: { Cookie: headers.get('cookie') ?? '' },
     })
+
     if (status === 200) {
       const nextRes = NextResponse.next()
       nextRes.cookies.set('isAuth', 'true')
       return nextRes
     }
+
+    // Refresh Token 로직짜기. 있던 쿠키 그대로 호출함. 리프레시 한걸 그대로 받음.
+    if (status === 401) {
+      const { status: refreshStatus, headers: refreshHeaders } = await fetch(
+        'https://api.sttock.co.kr/api/v1/user/me',
+        {
+          headers: { Cookie: headers.get('cookie') ?? '' },
+        }
+      )
+      if (refreshStatus === 201) {
+        const nextRes = NextResponse.next()
+        refreshHeaders.forEach((value, key) => {
+          if (key.toLowerCase() === 'set-cookie') {
+            nextRes.headers.append('Set-Cookie', value)
+          }
+        })
+        nextRes.cookies.set('isAuth', 'true')
+        return nextRes
+      }
+      console.log(refreshStatus)
+    }
+
     const redirectRes = NextResponse.redirect(
       new URL(`/login?continue=${encodeURIComponent(url)}`, url)
     )
